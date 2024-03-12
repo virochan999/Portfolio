ea16e1d7113a6716419f5793e6219364c15ec70a
Virochan Sharma <virochan999@gmail.com>
2024-03-12 12:26:51 +0530

    Fix: loading alignment  issue

--- src/app/loading.tsx
+++ src/app/loading.tsx
@@ -2,7 +2,7 @@ import React from "react"
 
 const loading = () => {
   return (
-    <div className="flex justify-center h-screen items-center">
+    <div className="flex justify-center flex-grow items-center">
       <span className="circle animate-loader"></span>
       <span className="circle animate-loader animation-delay-200"></span>
       <span className="circle animate-loader animation-delay-400"></span>
