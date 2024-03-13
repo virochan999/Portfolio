41460ef1772c51ecb5bd2811f9086c4058103b2c
Virochan Sharma <virochan999@gmail.com>
2024-03-13 19:26:54 +0530

    prisma migrations

---
+++ prisma/migrations/20240313135158_init/migration.sql
@@ -0,0 +1,11 @@
+-- AlterTable
+ALTER TABLE "Other" ADD COLUMN     "order" SERIAL NOT NULL;
+
+-- AlterTable
+ALTER TABLE "Professional" ADD COLUMN     "order" SERIAL NOT NULL;
+
+-- AlterTable
+ALTER TABLE "Project" ADD COLUMN     "order" SERIAL NOT NULL;
+
+-- AlterTable
+ALTER TABLE "Technical" ADD COLUMN     "order" SERIAL NOT NULL;
